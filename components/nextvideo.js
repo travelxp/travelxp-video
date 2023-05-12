92667d7e24195564ff2b6c54d6f4a4f55f1d0125
Prashant Chothani <prashant.chothani@travelxp.tv>
2023-05-12 15:48:59 +0530

    feat: videojs events using nuevo analytics done.

--- components/nextvideo.js
+++ components/nextvideo.js
@@ -25,14 +25,46 @@ export default function NextVideo(props) {
     useEffect(() => {
         require('../videojs/plugins/nuevo-dash.js');
         require('../videojs/nuevo.min.js');
-
-        //require('../videojs/plugins/videojs-chromecast.min.js');
         require('../videojs/plugins/videojs.events.js');
+        // require('../videojs/plugins/videojs.hotkeys.min.js');
+        // require('../videojs/plugins/videojs.thumbnails.js');
+        //require('../videojs/plugins/videojs-chromecast.min.js');
+
 
         if (videoEl == null) return
-        
+
         const player = videojs(videoEl, PlayerProps)
-        player.nuevo();
+        player.nuevo({ title: "video title", id: "video id" });
+
+        // Initialize Events plugin
+
+        player.events({ analytics: true });
+
+        // Track events
+
+        player.on("track", (e, data) => {
+            switch (data.event) {
+                case 'firstPlay': console.log('First Play', data); break;
+                case '10%': console.log('Progress 10%', data); break;
+                case '25%': console.log('Progress 25%', data); break;
+                case '50%': console.log('Progress 50%', data); break;
+                case '75%': console.log('Progress 75%', data); break;
+                case '90%': console.log('Progress 90%', data); break;
+                case 'buffered': console.log('Video buffered', data); break;
+                case 'paused': console.log('Video paused', data); break;
+                case 'resume': console.log('Video resumed', data); break;
+                case 'replay': console.log('Video replayed', data); break;
+                case 'enterFullscreen': console.log('Video entered fullscreen', data); break;
+                case 'exitFullscreen': console.log('Video exited fullscreen', data); break;
+                case 'seek': console.log('Video seeked', data); break;
+                case 'mute': console.log('Video muted', data); break;
+                case 'unmute': console.log('Video unmuted', data); break;
+                case 'resolutionChange': console.log('Video resolution changed', data); break;
+                case 'ended': console.log('Video completed', data); break;
+                case 'summary': console.log('Video summary', data); break; // Fires only when video ended
+            }
+        });
+
         // player.chromecast();
         return () => {
             player.dispose()
