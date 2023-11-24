7c6a320cb014350297256f7ce2f4818293e303e0
Prashant Chothani <prashant.chothani@travelxp.tv>
2023-11-24 10:16:26 +0530

    refactor: deleted code deploy files

--- codedeploy/after_install.sh
+++
@@ -1,9 +0,0 @@
-cd /var/www/html/travelxp.video
-sudo su
-rm -rf node_modules
-#rm -rf package-lock.json
-sudo npm install --save-dev
-#sudo npm install
-# 
-# sudo npm install
-# sudo npm run build
